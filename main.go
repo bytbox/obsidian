d15f47d5e6cddb8342c1c03111558e103609c2d0
Scott Lawrence <bytbox@gmail.com>
2010-07-30 17:27:16 -0400

    options

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"opts"
+)
 
+var port = opts.Option("p", "port", "the port to use", "8080")
+var blogroot = opts.Option("r", 
+	"blogroot", 
+	"the root directory for blog data",
+	"/var/obsidian")
+
+func main() {
+	opts.Description("lightweight http blog server")
+	opts.Parse()
 }
